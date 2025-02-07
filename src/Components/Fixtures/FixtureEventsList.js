9ef13e953dbd96b98db3c3ef5faad2cf315356f3
Kalum <kshorty@live.co.uk>
2025-02-07 13:39:54 +0000

    football icons in header improve

--- src/Components/Fixtures/FixtureEventsList.js
+++ src/Components/Fixtures/FixtureEventsList.js
@@ -10,7 +10,8 @@ export default function FixtureEventsList({
   const filteredEvents = events?.filter(
     (event) =>
       (eventTypes.includes(event.type) && event.detail === "Normal Goal") ||
-      event.detail === "Own Goal"
+      event.detail === "Own Goal" ||
+      (event.detail === "Penalty" && event.comment !== "Penalty Shootout")
   );
 
   return (
@@ -21,11 +22,41 @@ export default function FixtureEventsList({
 
         return goalAlign === "left" ? (
           <li key={index} className="fixure-events-list-item">
-            ⚽{event.player.name} {time}'
+            <img
+              key={index}
+              src={
+                event.detail === "Own Goal"
+                  ? "https://img.icons8.com/?size=100&id=LDze7ETPiEDu&format=png&color=FA5252"
+                  : event.detail === "Penalty"
+                  ? "https://img.icons8.com/?size=100&id=P54DbuwUMItC&format=png&color=000000"
+                  : "https://img.icons8.com/?size=100&id=cg5jSDHEKVtO&format=png&color=000000"
+              }
+              alt="Goal Icon"
+              width={20}
+              height={20}
+            />
+            {event.player.name} {time}'
           </li>
         ) : (
-          <li key={index} className="fixure-events-list-item">
-            {event.player.name} {time}' ⚽
+          <li
+            key={index}
+            className="fixure-events-list-item"
+            style={{ justifyContent: "end" }}
+          >
+            {event.player.name} {time}'{" "}
+            <img
+              key={index}
+              src={
+                event.detail === "Own Goal"
+                  ? "https://img.icons8.com/?size=100&id=LDze7ETPiEDu&format=png&color=FA5252"
+                  : event.detail === "Penalty"
+                  ? "https://img.icons8.com/?size=100&id=P54DbuwUMItC&format=png&color=000000"
+                  : "https://img.icons8.com/?size=100&id=cg5jSDHEKVtO&format=png&color=000000"
+              }
+              alt="Goal Icon"
+              width={20}
+              height={20}
+            />
           </li>
         );
       })}
