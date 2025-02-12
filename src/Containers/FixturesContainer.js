04f797c4f4d59876d59a8e1dee1b6eb54feb826c
Kalum <kshorty@live.co.uk>
2025-02-12 17:10:21 +0000

    Styles

--- src/Containers/FixturesContainer.js
+++ src/Containers/FixturesContainer.js
@@ -2,19 +2,5 @@ import React from "react";
 import FixturesList from "../Components/Fixtures/FixturesList";
 
 export default function FixturesContainer() {
-  // const dispatch = useDispatch();
-  // const fixtures = useSelector((state) => state.fixtures.fixtures);
-
-  // useEffect(() => {
-  //   // Only fetch fixtures if the store is empty
-  //   if (
-  //     !fixtures?.previousFixtures.length &&
-  //     !fixtures?.upcomingFixtures.length &&
-  //     !fixtures?.latestFixture
-  //   ) {
-  //     dispatch(fetchFixtures());
-  //   }
-  // }, [dispatch, fixtures]);
-
   return <FixturesList />;
 }
