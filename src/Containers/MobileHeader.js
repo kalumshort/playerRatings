f3b44dad74099269fe34b2f359fceee01ee9c9f7
Kalum <kshorty@live.co.uk>
2025-04-24 17:00:21 +0100

    Added the dark logo for the light mode header

--- src/Containers/MobileHeader.js
+++ src/Containers/MobileHeader.js
@@ -4,12 +4,15 @@ import { AppBar, Toolbar, IconButton, Drawer, styled } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { DrawerContentComponent } from "./Header";
 
+import blackLogo from "../assets/logo/11votes-logo-clear-nobg-black.png";
 import whiteLogo from "../assets/logo/11votes-nobg-clear-white.png";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "../Components/Theme/ThemeContext";
 
 export default function MobileHeader() {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const navigate = useNavigate();
+  const theme = useTheme();
 
   const Logo = styled("img")({
     height: "50px",
@@ -29,11 +32,13 @@ export default function MobileHeader() {
           sx={{
             display: "flex",
             justifyContent: "space-between",
+            alignItems: "center",
             gap: 4,
+            height: "70px",
           }}
         >
           <Logo
-            src={whiteLogo}
+            src={theme.themeMode === "dark" ? whiteLogo : blackLogo}
             alt="11Votes Logo"
             onClick={() => navigate(`/`)}
           />
