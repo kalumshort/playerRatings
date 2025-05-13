35a948b53571e840bff54f10d4e3699aed52d1a5
Kalum <kshorty@live.co.uk>
2025-05-13 17:43:31 +0100

    cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-restricted-globals */
 
 // src/service-worker.js
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+
+// This ensures Workbox will precache the app's assets.
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Caching strategy for images (Stale-While-Revalidate)
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new StaleWhileRevalidate()
+);
+
+// Caching strategy for other resources (Network First)
+registerRoute(
+  ({ request }) => request.destination !== "image",
+  new StaleWhileRevalidate()
+);
