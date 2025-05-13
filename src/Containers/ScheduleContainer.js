5136022bf00721df5c7524e1828cf181f7f8ef6f
Kalum <kshorty@live.co.uk>
2025-05-13 20:55:44 +0100

    improved schedule on homepage

--- src/Containers/ScheduleContainer.js
+++ src/Containers/ScheduleContainer.js
@@ -70,7 +70,7 @@ export default function ScheduleContainer({
         });
       }
     }
-  }, [latestFixture, limitedFixtures]);
+  }, [latestFixture, limitedFixtures, scrollOnLoad]);
 
   const handleFixtureClick = (matchId) => {
     navigate(`/fixture/${matchId}`);
