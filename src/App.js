587402a47a93470be33154dd1fe571dd65f58321
Kalum <44569389+kalumshort@users.noreply.github.com>
2025-06-05 18:19:02 +0100

    Remove unused counter example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-// import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { increment, decrement, incrementByAmount } from "./redux/counterSlice";
-
-// function App() {
-//   const count = useSelector((state) => state.counter.value);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "50px" }}>
-//       <h1>Redux Counter</h1>
-//       <h2>{count}</h2>
-//       <button onClick={() => dispatch(increment())}>Increment</button>
-//       <button onClick={() => dispatch(decrement())}>Decrement</button>
-//       <button onClick={() => dispatch(incrementByAmount(5))}>
-//         Increment by 5
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
