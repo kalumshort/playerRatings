644475009bd70e08d5f0d4f6fbea7d9fe4a77c4e
Kalum <kshorty@live.co.uk>
2025-06-12 17:32:23 +0100

    cache

--- src/App.js
+++ src/App.js
@@ -117,12 +117,11 @@ function App() {
     return <Spinner />;
   }
 
-  // index.js or App.js
-  // if ("serviceWorker" in navigator) {
-  //   window.addEventListener("load", () => {
-  //     navigator.serviceWorker.register("/service-worker.js");
-  //   });
-  // }
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js");
+    });
+  }
 
   return (
     <GlobalContainer>
