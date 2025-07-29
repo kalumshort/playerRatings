0580f25ca96b8e1bca298a60a4e36c6ab9c7fedf
Kalum <kshorty@live.co.uk>
2025-07-29 11:44:56 +0100

    fixed post kick off predictions

--- src/Components/Fixtures/Fixture-Components/PostKickoffPredictions.js
+++ src/Components/Fixtures/Fixture-Components/PostKickoffPredictions.js
@@ -2,11 +2,19 @@ import React from "react";
 import ScorePredictionResults from "./ScorePredictionResults";
 
 import LineupPredictor from "./LineupPredicter/LineupPredictor";
+import { selectUserMatchData } from "../../../Selectors/userDataSelectors";
+import { useSelector } from "react-redux";
 
 export default function PostKickoffPredictions({ fixture }) {
+  const usersMatchData = useSelector(selectUserMatchData(fixture.id));
+
+  const storedUsersPredictedScore = usersMatchData?.ScorePrediction;
   return (
-    <div className="pkopredictions-container">
-      <ScorePredictionResults fixture={fixture} />
+    <div className="pkopredictions-container containerMargin">
+      <ScorePredictionResults
+        fixture={fixture}
+        storedUsersPredictedScore={storedUsersPredictedScore}
+      />
       <LineupPredictor fixture={fixture} readOnly={true} />
     </div>
   );
