cd2910bf4b1995298303e54e9e450c293524247e
Kalum <kshorty@live.co.uk>
2026-03-04 12:15:20 +0700

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Turbopack is now native in Next.js 16+.
-  // It will automatically handle server-side vs client-side code
-  // and tree-shake the Admin SDK out of the browser bundle for you.
   images: {
     remotePatterns: [
       {
