3462c2d885b661225c99d2c7e53e431b7b3f3feb
Kalum <kshorty@live.co.uk>
2026-03-05 19:41:07 +0700

    client variables

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -5,19 +5,16 @@ import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions"; // 1. Import the service
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.CLIENT_API_KEY,
+  authDomain: process.env.CLIENT_AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.CLIENT_STORAGE_BUCKET,
+  messagingSenderId: process.env.CLIENT_MESSAGING_SENDER_ID,
+  appId: process.env.CLIENT_APP_ID,
+  measurementId: process.env.CLIENT_MEASUREMENT_ID,
 };
 if (typeof window !== "undefined") {
-  console.log(
-    "DEBUG_API_KEY_LENGTH:",
-    process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.length,
-  );
+  console.log("DEBUG_API_KEY_LENGTH:", process.env.CLIENT_API_KEY?.length);
 }
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
